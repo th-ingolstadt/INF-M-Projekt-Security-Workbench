d0a3e3cb433d33938d1c036ec6efc651ce21521c
RobertZisler <roz3912@thi.de>
2017-11-05 19:49:17 +0100

    added local db to web app

--- Projekte/BrokenWebApplication_v1/AdminLTE1/Properties/AssemblyInfo.cs
+++ Projekte/BrokenWebApplication_v1/AdminLTE1/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AdminLTE1")]
+[assembly: AssemblyTitle("SecurityWorkBenchTutorials")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AdminLTE1")]
-[assembly: AssemblyCopyright("Copyright © 2017 ")]
+[assembly: AssemblyProduct("SecurityWorkBenchTutorials")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
