e9ab1e1456ca49ee2f7262bff938cd598f403f74
RobertZisler <roz3912@thi.de>
2017-11-06 10:21:49 +0100

    Implemented Local stored xss demo with guestbook entry as example

--- Projekte/BrokenWebApplication_v1/AdminLTE1/Scripts/js/guestbook.js
+++ Projekte/BrokenWebApplication_v1/AdminLTE1/Scripts/js/guestbook.js
@@ -1,176 +1,61 @@
-﻿var id = 1; 
+﻿function checkCookiesForLoggedinUser() {
+    var usrnameCookie = getCookie("username");
+    var pwdCookie = getCookie("password"); 
 
-function EntryDTO(id, date, content) {
-    this.id = id;
-    this.date = date;
-    this.content = content
-
-    this.pruefe = function () {
-        if (this.content === "") {
-            throw "ERROR! Eines der Elemente ist nicht initialisiert";
-        } else if (this.date < 0) {
-            throw "ERROR! Die Postleitzahl darf nicht negativ sein!";
-        }
-
-    }
-    this.toString = function () {
-        return "{" + this.id + ", " +
-            this.date + ", " +
-            this.content + "}";
-    }
-}
-
-function EntryDAO() {
-    this.entryArray = [];
-    
-    this.speichern = function () {
-        localStorage.setItem('entryDAO', JSON.stringify(this)); // stringify -> JavaScript obj zu JSON Zeichenkette
-    };
-
-    this.laden = function () {
-        var gespeichertesDAOalsString = localStorage.getItem('entryDAO');
-        var gespeichertesDAO = JSON.parse(gespeichertesDAOalsString); // parse// JSON zu JavaScript
-
-        if (gespeichertesDAO != null)
-            this.entryArray = gespeichertesDAO.entryArray;
-
-        for (i = 0; i < this.entryArray.length; ++i) {
-            this.entryArray[i] = new PersonDTO(
-                this.entryArray[i].id,
-                this.entryArray[i].date,
-                this.entryArray[i].content);
-        }
-
-        console.log('gespeichertesDAO: ', gespeichertesDAO);
-    };
-
-	/*
-	 * Hilfsfunktionen
-	 */
-	/*
-	 * Liefert true, falls die übergebene Personenbeschreibung 'person' die Präfixe 
-	 * für 'name' und 'ort' besitzt bzw. 'name' und 'ort' leer sind (keine Prüfung).
-	 * Sonst wird false zurückgegeben.
-	 */
-    this.filter = function (person, name, ort) {
-        // *** (2) ***
-        if (name === undefined && ort === undefined) {
-            return true;
-        } else if (person.name.substr(0, name.length) === name &&
-            person.ort.substr(0, ort.length) === ort) {
-            return true;
-        }
-
-        return false;
-    };
-
-    this.findeAlle = function () {
-        this.laden();
-        var ergebnis = [];
-        var j = 0;
-
-        for (i = 0; i < this.entryArray.length; ++i) {
-            if (this.entryArray[i].id != -1) {
-                ergebnis[j++] = this.entryArray[i];
-            }
-        }
-
-        ergebnis.sort(
-            function (p1, p2) {
-                return p1.id - p2.id;
-            }
-        );
-
-        return ergebnis;
-    };
-
-    this.newEntry = function (entry) {
-        this.laden();
-
-        for (i = 0; i < this.entryArray.length; ++i) {
-            if (this.entryArray[i].id == -1) {
-                break;
-            }
-        }
-        entry.id = i;
-        this.entryArray[entry.id] = person;
-        this.speichern();
-    };
-
-    this.actualizeEntry = function (entry) {
-        this.laden();
-        if (this.findePersonZuId(entry.id) !== "undefined") {
-            this.entryArray[entry.id] = person;
-            this.speichern();
-        }
-    };
-
-    this.loeschePerson = function (id) {
-        // *** (4) ***
-        // First step: Search for the person
-
-        // var index = 0;
-        var i;
-        for (i = 0; i < this.entryArray.length; i++) {
-            if (this.entryArray[i].id === id) {
-                this.entryArray[i].id = -1;
-                this.speichern();
-                break;
-            }
-        }
-		/*this.laden();
-		this.personArray[index].id = -1;
-		this.speichern();*/
-		/* var index = this.findePersonZuId(id);
-		if ( index !== "undefined") {
-			this.personArray[index].id = -1;
-		//	delete this.personArray[index];
-		//	this.personArray.splice(index, 1);
-		//	localStorage.removeItem(personDAO.personArray[index]);
-			this.speichern();
-		} else {
-			alert("ERROR");
-		}*/
-    };
-}
-
-function gibPersonDAO() {
-    var dao = "undefined";
-
-    if (typeof (Storage) !== "undefined") {
-        dao = new EntryDAO();
-        if (localStorage['entryDAO']) {
-            try {
-                dao.laden();
-                for (i = 0; i < dao.entryArray.length; ++i) {
-                    var p = dao.entryArray[i];
-                    console.log(p.toString());
-                }
-
-            } catch (error) {
-                alert(error);
-            }
-        }
+    alert(usrnameCookie + "--Cookie:--" + pwdCookie); 
+    if (usrnameCookie === "123" && pwdCookie === "123") {
+        document.getElementById("guestbook").removeAttribute("hidden");
+        getContentOfGuestbook(); 
     } else {
-        alert("Sorry, your browser does not support web storage…");
+        alert("hidden...");
     }
-
-    return dao;
 }
 
-var entryDAO = gibPersonDAO();
+function getContentOfGuestbook() {
+    /*
+    var db = Database.Open("Database1");
+    var selectQueryString = "SELECT * FROM Guestbook";
 
-function storeEntry() {
-    var date = new Date(); 
-    var entry = new EntryDTO(id++,
-        document.getElementById("guestbookEntry").value,
-        date.toDateString
-    );
+    alert("before tableref");
+    var tableRef = document.getElementById('guestbook').getElementsByTagName('tbody')[0];
+    alert("after tableref"); 
+    var row; 
+    foreach(row in db.Query(selectQueryString))
+    {
+        alert("inside forecach"); 
+        var newRow = tableRef.insertRow(tableRef.rows.length);
+        var newCell = newRow.insertCell(0);
+        var newText = document.createTextNode(row.username);
+        newCell.appendChild(newText);
+        var dateCell = newRow.insertCell(1);
+        var dateText = document.createTextNode(row.date);
+        dateCell.appendChild(dateText); 
+        var commentCell = newRow.insertCell(2);
+        var commentText = document.createTextNode(row.comment);
+        commentCell.appendChild(commentText); 
+    } */
+}
 
-    try {
-        entry.pruefe(); 
-        entry
-    } catch (errorMsg) {
-        alert(errorMsg)
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
     }
+    return "";
+}
+
+function checkLoginInput() {
+    var username = document.getElementById("usrname").value;
+    var password = document.getElementById("pwd").value;
+    alert(username + "--" + password); 
+    document.cookie = "username=" + username;
+    document.cookie = "password=" + password; 
 }
