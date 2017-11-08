834f2acd9f3fd5dd180423d6d2965b9bd6678d05
RobertZisler <roz3912@thi.de>
2017-11-08 13:52:56 +0100

    further impl

--- Projekte/BrokenWebApplication_v1/AdminLTE1/Scripts/js/guestbook.js
+++ Projekte/BrokenWebApplication_v1/AdminLTE1/Scripts/js/guestbook.js
@@ -2,38 +2,13 @@
     var usrnameCookie = getCookie("username");
     var pwdCookie = getCookie("password"); 
 
-    alert(usrnameCookie + "--Cookie:--" + pwdCookie); 
-    if (usrnameCookie === "123" && pwdCookie === "123") {
+    if ((usrnameCookie === "123" && pwdCookie === "123")
+        || (usrnameCookie === "admin" && pwdCookie === "admin")
+        || (usrnameCookie === "user" && pwdCookie === "password1")) {
+        document.getElementById("labelGuestbook").innerHTML = "Hinterlasse eine Eintrag in " + usrnameCookie + "'s Gästebuch";
+        document.getElementById("guestbookHeader").innerHTML = usrnameCookie + "'s Gästebuch"; 
         document.getElementById("guestbook").removeAttribute("hidden");
-        getContentOfGuestbook(); 
-    } else {
-        alert("hidden...");
-    }
-}
-
-function getContentOfGuestbook() {
-    /*
-    var db = Database.Open("Database1");
-    var selectQueryString = "SELECT * FROM Guestbook";
-
-    alert("before tableref");
-    var tableRef = document.getElementById('guestbook').getElementsByTagName('tbody')[0];
-    alert("after tableref"); 
-    var row; 
-    foreach(row in db.Query(selectQueryString))
-    {
-        alert("inside forecach"); 
-        var newRow = tableRef.insertRow(tableRef.rows.length);
-        var newCell = newRow.insertCell(0);
-        var newText = document.createTextNode(row.username);
-        newCell.appendChild(newText);
-        var dateCell = newRow.insertCell(1);
-        var dateText = document.createTextNode(row.date);
-        dateCell.appendChild(dateText); 
-        var commentCell = newRow.insertCell(2);
-        var commentText = document.createTextNode(row.comment);
-        commentCell.appendChild(commentText); 
-    } */
+    } 
 }
 
 function getCookie(cname) {
@@ -42,10 +17,10 @@ function getCookie(cname) {
     var ca = decodedCookie.split(';');
     for (var i = 0; i < ca.length; i++) {
         var c = ca[i];
-        while (c.charAt(0) == ' ') {
+        while (c.charAt(0) === ' ') {
             c = c.substring(1);
         }
-        if (c.indexOf(name) == 0) {
+        if (c.indexOf(name) === 0) {
             return c.substring(name.length, c.length);
         }
     }
@@ -55,7 +30,6 @@ function getCookie(cname) {
 function checkLoginInput() {
     var username = document.getElementById("usrname").value;
     var password = document.getElementById("pwd").value;
-    alert(username + "--" + password); 
     document.cookie = "username=" + username;
     document.cookie = "password=" + password; 
 }
