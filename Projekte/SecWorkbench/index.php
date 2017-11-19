78dacf2257fe8b3dcc1c96e2d11ae19af085d6a1
Marco Egner <egner.marco@web.de>
2017-11-19 17:34:55 +0100

    Layout wird geladen; PHP-Code wird nun richtig interpretiert; Layout besteht nun aus einem oberen und unteren Teil, der jeweils um die Content-Files angefügt werden muss

--- Projekte/SecWorkbench/index.php
+++ Projekte/SecWorkbench/index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-            include ("./StaticSites/Index.html");
+            include ("./Index.html");
         ?>
     </body>
 </html>
