11df2353ba1442057bbf338eebf2d969cee3661f
Michi344 <32903245+Michi344@users.noreply.github.com>
2017-12-04 16:19:58 +0100

    Update FirstExample.c
    Veraenderung der Ausgabe bei flascher und richtiger Eingabe

--- Projekte/BufferOverflow/FirstExample.c
+++ Projekte/BufferOverflow/FirstExample.c
@@ -21,10 +21,12 @@ int main(int argc, char *argv[])
 	if(authflag != 0)					//check for Authentication
 	{
 		char* Solution = malloc(sizeof(char)*15);
-		char* a = "XX"; char* b = "XX"; char* c = "XX"; char* d = "XX"; char* e = "XX"; char* f =  "XX";
+		//char* a = "XX"; char* b = "XX"; char* c = "XX"; char* d = "XX"; char* e = "XX"; char* f =  "XX";
+		char* a = "DE"; char* b = "12"; char* c = "34"; char* d = "56"; char* e = "67"; char* f =  "78";
 
 		strcat(Solution,a); strcat(Solution,b); strcat(Solution,c); strcat(Solution,d); strcat(Solution,e); strcat(Solution,f);
-		printf("Das Lösungswort ist : %s\n", Solution);
-	}
+		
+		printf("Zugriff gewaehrt! Die Kontodaten von Max Mustermann sind: %s\n", Solution);
+	}else printf("Ungueltige Eingabe! -> Zugriff verweigert!\n");
 	
 }
