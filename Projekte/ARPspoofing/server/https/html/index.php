815ee3d8d36672a3efc4ed0991bc8c9462183f49
sakstone <sak6831@thi.de>
2017-12-06 11:16:29 +0000

    In ARPSpoofing Server -> new dir for http and https

---
+++ Projekte/ARPspoofing/server/https/html/index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>ARP Spoofing Website</title>
+</head>
+
+<h1>
+Website für ARP-Spoofing
+</h1>
+
+<p>
+Eine beliebige Webseite für das ARP-Spoofing Szenario.
+</p>
+
+</html>
