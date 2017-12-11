ea26b9e291a4965fb79866148dc61b98139fa334
RobertZisler <roz3912@thi.de>
2017-12-11 15:31:31 +0100

    actualized sql script for testing purpose

--- Projekte/SQLInjection/server/initializeDB.sql
+++ Projekte/SQLInjection/server/initializeDB.sql
@@ -6,11 +6,18 @@ create table vulnerableDB.secretUserData(userId int primary key auto_increment, 
 
 # insert some values in the database table vulnerableDB.secretUserData
 insert into vulnerableDB.secretUserData (userId, userName, password) values ('1', 'Douglas Adams', 'DontPanic!');
-insert into vulnerableDB.secretUserData (userId, userName, password) values ('2', 'Harry Potter', 'CaputDraconis'); 
+insert into vulnerableDB.secretUserData (userId, userName, password) values ('2', 'Harry Potter', 'CaputDraconis');
 insert into vulnerableDB.secretUserData (userId, userName, password) values ('3', 'James T. Kirk', 'BeamMeUpScotty');
 insert into vulnerableDB.secretUserData (userId, userName, password) values ('4', 'Grumpy Cat', 'No!');
 insert into vulnerableDB.secretUserData (userId, userName, password) values ('5', 'Dalek', 'Exterminate!');
 insert into vulnerableDB.secretUserData (userId, userName, password) values ('6', 'The Doctor', 'Allons-y');
 insert into vulnerableDB.secretUserData (userId, userName, password) values ('7', 'Deadpool', 'Chimichanga');
 
+create table vulnerableDB.sqlInjectionRanking(userid int primary key auto_increment, user varchar(20), punkte int);
 
+insert into vulnerableDB.sqlInjectionRanking (userid, user, punkte) values ('1', 'Daniel', '50');
+insert into vulnerableDB.sqlInjectionRanking (userid, user, punkte) values ('2', 'Werner', '77');
+insert into vulnerableDB.sqlInjectionRanking (userid, user, punkte) values ('3', 'Mr. Robot', '66');
+insert into vulnerableDB.sqlInjectionRanking (userid, user, punkte) values ('4', 'Bernd', '44');
+insert into vulnerableDB.sqlInjectionRanking (userid, user, punkte) values ('5', 'root', '42');
+insert into vulnerableDB.sqlInjectionRanking (userid, user, punkte) values ('6', 'admin', '97');
