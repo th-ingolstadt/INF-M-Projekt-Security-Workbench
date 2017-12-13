b637274937dedca414ff4d0f2590dff2a1a460ba
Sebastian Huettersen <sebastian@huettersen.com>
2017-12-13 11:49:43 +0100

    Install Mod
    Changes to be committed:
    	modified:   raspiDockerInstall.sh
    	modified:   raspiService.sh

--- Projekte/InstallScripts/raspiDockerInstall.sh
+++ Projekte/InstallScripts/raspiDockerInstall.sh
@@ -2,27 +2,37 @@
 # Script for Building Docker Container
 # Build Docker Container
 # ARP-Spoofing Container NEWS
+project_path=$(realpath ..)
+
 echo "ARP HTTP Webserver wird gebaut"
-export ARP_WEB_RES_PATH='/home/pi/INF-M-Projekt-Security-Workbench/Projekte/ARPspoofing/server/http/html'
+export ARP_WEB_RES_PATH=$project_path/ARPspoofing/server/http/html
 chmod +x ../ARPspoofing/server/http/arphttpDockerControl.sh
 ../ARPspoofing/server/http/arphttpDockerControl.sh start
 
 echo "ARP HTTPS Webserver wird gebaut"
 # ARP-Spoofing Container HTTPS NO-HSTS
-export ARP_WEB_RES_PATH='/home/pi/INF-M-Projekt-Security-Workbench/Projekte/ARPspoofing/server/https/html'
+export ARP_WEB_RES_PATH=$project_path/ARPspoofing/server/https/html
 chmod +x ../ARPspoofing/server/https/arphttpsDockerControl.sh
 ../ARPspoofing/server/https/arphttpsDockerControl.sh start
 
 # ARP-Spoofing Container HTTPS HSTS
-#export ARP_WEB_RES_PATH='/home/pi/INF-M-Projekt-Security-Workbench/Projekte/ARPspoofing/server/https/html'
+#export ARP_WEB_RES_PATH=$project_path/ARPspoofing/server/https/html
 #chmod +x ../ARPspoofing/server/https/arphttpsDockerControl.sh
 #../ARPspoofing/server/https/arphttpsDockerControl.sh start
 echo "Tutorial Webseite wird gebaut"
 # Tutorial Container 
-export WEB_RES_PATH='/home/pi/INF-M-Projekt-Security-Workbench/Projekte/SecWorkbench/html'
+export WEB_RES_PATH=$project_path/SecWorkbench/html
 export WEB_RES_IP='172.26.112.1:80:80'
 chmod +x ../SecWorkbench/tutorialDockerControl.sh
 ../SecWorkbench/tutorialDockerControl.sh start
+
+# Linux Keylogger Container
+export LINUX_KEYLOGGER_WEB_RES_PATH=$project_path/Keylogger/wifi_snatcher_DigiSpark/linux/html
+../Keylogger/wifi_snatcher_DigiSpark/linux/keyloggerLinuxDockerControl.sh start
+# Windows Keylogger container
+export WINDOS_KEYLOGGER_WEB_RES_PATH=$project_path/Keylogger/wifi_snatcher_DigiSpark/windows/html
+../Keylogger/wifi_snatcher_DigiSpark/windows/keyloggerDockerControl.sh start
+
 echo "############################################################"
 echo "Installion 2/2 Complete NO-Reboot needed."
 echo "Info über WLAN-AP: NAME:PW"
